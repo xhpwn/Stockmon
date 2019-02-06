b474654cf4ec75ba36aa1fd66a3423355262278a
xhpwn <saxena20@purdue.edu>
2019-02-06 09:25:15 -0500

    Registration route setup

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,1 +1,31 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
+
+const router = express.Router();
+
+router.post("/register", (req, res, next) => {
+    bcrypt.hash(req.body.password, 10)
+        .then(hash =>  {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            password: hash
+        });
+        user.save()
+        .then(result => {
+            res.status(200).json({
+                message: 'Registration Successful.',
+                result: result
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
+    });
+});
+
+module.exports = router;
