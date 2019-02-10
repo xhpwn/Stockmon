5e8fdab31d58216991b01f5d8eb45d9eef547314
$Avadhoot Joshi <$joshi41@purdue.edu>
2019-02-10 17:42:29 -0500

    Pushing changes made so far

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-
+import { ChartsModule } from 'ng2-charts';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
--- src/app/stocks/stocks.component.ts
+++ src/app/stocks/stocks.component.ts
@@ -7,6 +7,14 @@ import { StockService } from '../stockservice';
   templateUrl: './stocks.component.html',
   styleUrls: ['./stocks.component.css']
 })
+
+chartData = [
+  { data: [330, 600, 260, 700], label: 'Account A' },
+  { data: [120, 455, 100, 340], label: 'Account B' },
+  { data: [45, 67, 800, 500], label: 'Account C' }
+];
+chartLabels = ['January', 'February', 'Mars', 'April'];
+ 
 export class StocksComponent implements OnInit {
 
   inFocus;
