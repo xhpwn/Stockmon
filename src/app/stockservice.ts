826598ba50b1fbb1144f023148488e2f93cbe64c
Shaan Hoda <shaan.hoda@gmail.com>
2019-02-21 04:19:54 -0500

    Added error messages for infocus, gainers, losers, following, and description

--- src/app/stockservice.ts
+++ src/app/stockservice.ts
@@ -7,7 +7,8 @@ import { AuthService } from "./authservice";
 
 export class StockService {
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+  }
 
   getInfocus() {
     return this.http.get("http://localhost:3000/api/stocks/getinfocus");
@@ -24,11 +25,11 @@ export class StockService {
   getTest() {
     return this.http.get("http://localhost:3000/api/stocks/test");
   }
-  
-  getChartData(symbol: string, timeFrame: string) {
+
+  /*getChartData(symbol: string, timeFrame: string) {
     let query = "http://localhost:3000/api/stocks/getChartData?symbol=" + symbol + "&time=" + timeFrame;
     return this.http.get(query);
-}
+  }*/
 
   getChartData(symbol: string, timeFrame: string) {
     let query = "http://localhost:3000/api/stocks/getChartData?symbol=" + symbol + "&time=" + timeFrame;
@@ -36,7 +37,7 @@ export class StockService {
   }
 
   getFollowingList(userid) {
-    const body = { 'id': userid };
+    const body = {'id': userid};
     return this.http.post("http://localhost:3000/api/stocks/getfollowing", body);
   }
 
@@ -44,7 +45,8 @@ export class StockService {
     return this.http.get("http://localhost:3000/api/stocks/getdescription");
   }
 
-  getPrice(symbol : string) {
+  getPrice(symbol: string) {
     let query = "http://localhost:3000/api/stocks/getPrice?symbol=" + symbol;
     return this.http.get(query);
   }
+}
