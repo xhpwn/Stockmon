3ece276d1670d0eac182080eaa8fe410083104e0
AvadhootJ <avadhootjoshi55@gmail.com>
2019-03-28 01:46:41 -0400

    Added cryptodata request function

--- backend/routes/stocks.js
+++ backend/routes/stocks.js
@@ -10,6 +10,23 @@ const router = express.Router();
 
 /////////////////// Cryptocurrency backend /////////////////////////
 
+router.get("/getcryptodata", (req, res, next) => {
+  let data = new Array();
+  let url = "https://min-api.cryptocompare.com/data/histominute?fsym="+ element.symbol + "&tsym=USD&limit=5&api_key=70e0677660c6d62a72896f47363843d2cc001f0545607cf089d4fd63645f868e";
+  axios.get(url)
+    .then(response => {
+      response.data.forEach(element => {
+        let obj = { "label": element.time, "value": element.close }
+        data.push(obj)
+      });
+      res.status(200).send(json.stringify(data));
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+
 router.post("/getCryptPortfolio", (req, res, next) => {
   User.findById(req.body.id, function (err, obj) {
     let newData = [];
