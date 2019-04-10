0a2b4676cbdf904599ae083869aad0ba7dafd3e4
AvadhootJ <avadhootjoshi55@gmail.com>
2019-04-10 16:57:43 -0400

    Story #51 Complete. Will be moved later

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  n =  new Date();
+  dayofWeek = this.n.getDay(); // 0 -6 0 = SUnday 1 = Monday 2 = Tuesday...
+  hourofDay = this.n.getHours(); // 24 hour format
+  marketStatus = "open";
+
   constructor() { }
 
   ngOnInit() {
-  }
+    console.log(this.dayofWeek);
 
+    if((this.dayofWeek == 0 && this.hourofDay < 17) || (this.dayofWeek == 5 && this.hourofDay >= 16) || (this.dayofWeek == 6)){
+      this.marketStatus == "closed";
+    }
+  }
+  
 }
+
+
+
